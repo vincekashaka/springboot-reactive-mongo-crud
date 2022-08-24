90c4d214506d84514d7485c29182ca1a112d5282
Vincent Kashaka <vincekashaka@gmail.com>
2022-08-24 18:37:04 +0200

    added service class

---
+++ src/main/java/com/vince/reactive/service/ProductService.java
@@ -0,0 +1,41 @@
+package com.vince.reactive.service;
+
+import com.vince.reactive.dto.ProductDto;
+import com.vince.reactive.entity.Product;
+import com.vince.reactive.repository.ProductRepository;
+import com.vince.reactive.utils.AppUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Range;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+
+
+    // Get API
+
+    public Flux<ProductDto> getProducts(){
+        return productRepository.findAll().map(AppUtils::entityToDto);
+    }
+
+    public Mono<ProductDto> getProductById(String id){
+        return productRepository.findById(id).map(AppUtils::entityToDto);
+    }
+
+    public Flux<ProductDto> getProductInRange(double min, double max){
+        return productRepository.findByPriceBetween(Range.closed(min, max));
+    }
+
+    // POST API
+    public Mono<ProductDto> saveProduct(Mono<ProductDto> productDtoMono){
+       return productDtoMono.map(AppUtils::dtoToEntity)
+                .flatMap(productRepository::insert)
+                .map(AppUtils::entityToDto);
+    }
+}
