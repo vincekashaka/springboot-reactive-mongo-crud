2f8d71a71697bfa40f630a8d735f7fce08ae039c
Vincent Kashaka <vincekashaka@gmail.com>
2022-08-24 18:37:45 +0200

    update repository

--- src/main/java/com/vince/reactive/repository/ProductRepository.java
+++ src/main/java/com/vince/reactive/repository/ProductRepository.java
@@ -1,4 +1,13 @@
 package com.vince.reactive.repository;
 
-public interface ProductRepository {
+import com.vince.reactive.dto.ProductDto;
+import com.vince.reactive.entity.Product;
+import org.springframework.data.domain.Range;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+
+@Repository
+public interface ProductRepository extends ReactiveMongoRepository<Product, String> {
+    Flux<ProductDto> findByPriceBetween(Range<Double> priceRange);
 }
